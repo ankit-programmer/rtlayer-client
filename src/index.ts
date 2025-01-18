a9f969e05d558edb0071d3742629eca3ecfee9de
walkover-ankit <ankitkumar@whozzat.com>
2025-01-18 17:12:26 +0530

    Bug Fix

--- src/index.ts
+++ src/index.ts
@@ -53,7 +53,7 @@ class WebSocketClient {
                 for await (const message of this.queue.messages()) {
                     if (this.ws?.readyState === WebSocket.OPEN && message) {
                         this.ws.send(message);
-                        await delay(50);
+                        await delay(100);
                     }
                 }
             };
